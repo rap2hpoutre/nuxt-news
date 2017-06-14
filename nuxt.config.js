61c83b6c6c11e802abd608a7af963853f7ce1621
rap2h <raphaelht@gmail.com>
2017-06-14 15:55:38 +0200

    hop

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'starter',
+    title: 'Nuxt News - by @rap2h',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
